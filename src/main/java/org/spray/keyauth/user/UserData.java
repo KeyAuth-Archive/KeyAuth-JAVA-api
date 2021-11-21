852b3b12d2d17fec5c8a9540df0a1adc9e3d2626
SprayDown <67624548+SprayDown@users.noreply.github.com>
2021-11-21 16:06:48 +0600

    nov 20 update

--- src/main/java/org/spray/keyauth/user/UserData.java
+++ src/main/java/org/spray/keyauth/user/UserData.java
@@ -8,10 +8,8 @@ public class UserData {
 	private String username;
 	private String subscription;
 	private String expiry;
-	private String key;
-	private UserStatus status;
 
-	public UserData(JSONObject json, String key, String status) {
+	public UserData(JSONObject json) {
 		JSONObject info = json.getJSONObject("info");
 
 		JSONObject object = info.getJSONObject("subscriptions");
@@ -21,14 +19,6 @@ public class UserData {
 		this.username = object.getString("username");
 		this.subscription = subObject.getString("subscription");
 		this.expiry = subObject.getString("expiry");
-		this.key = key;
-		this.status = UserStatus.valueOf(status);
-		
-//		System.out.println("Username: " + username);
-//		System.out.println("subscription: " + subscription);
-//		System.out.println("expiry: " + expiry);
-//		System.out.println("key: " + key);
-//		System.out.println("status: " + status);
 	}
 
 	public String getUsername() {
@@ -41,18 +31,6 @@ public class UserData {
 
 	public String getExpiry() {
 		return expiry;
-	}
-
-	public String getKey() {
-		return key;
-	}
-	
-	public UserStatus getStatus() {
-		return status;
-	}
-	
-	public enum UserStatus {
-		SUCCESS, ERROR;
 	}
 
 }
