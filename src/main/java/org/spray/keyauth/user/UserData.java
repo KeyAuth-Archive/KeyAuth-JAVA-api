3aef5a43da71e36fdead5495cb6002f7bff930cf
SprayDown <67624548+SprayDown@users.noreply.github.com>
2021-11-21 16:03:32 +0600

    nov 20 update

--- src/main/java/org/spray/keyauth/user/UserData.java
+++ src/main/java/org/spray/keyauth/user/UserData.java
@@ -6,29 +6,33 @@ import org.json.JSONObject;
 public class UserData {
 
 	private String username;
-	private String ip;
 	private String subscription;
 	private String expiry;
+	private String key;
+	private UserStatus status;
 
-	public UserData(JSONObject json) {
+	public UserData(JSONObject json, String key, String status) {
 		JSONObject info = json.getJSONObject("info");
 
-		this.username = info.getString("username");
-		this.ip = info.getString("ip");
-
-		JSONArray subArray = info.getJSONArray("subscriptions");
+		JSONObject object = info.getJSONObject("subscriptions");
+		JSONArray subArray = object.getJSONArray("subscriptions");
 		JSONObject subObject = subArray.getJSONObject(0);
 		
+		this.username = object.getString("username");
 		this.subscription = subObject.getString("subscription");
 		this.expiry = subObject.getString("expiry");
+		this.key = key;
+		this.status = UserStatus.valueOf(status);
+		
+//		System.out.println("Username: " + username);
+//		System.out.println("subscription: " + subscription);
+//		System.out.println("expiry: " + expiry);
+//		System.out.println("key: " + key);
+//		System.out.println("status: " + status);
 	}
 
 	public String getUsername() {
 		return username;
-	}
-
-	public String getIp() {
-		return ip;
 	}
 
 	public String getSubscription() {
@@ -37,6 +41,18 @@ public class UserData {
 
 	public String getExpiry() {
 		return expiry;
+	}
+
+	public String getKey() {
+		return key;
+	}
+	
+	public UserStatus getStatus() {
+		return status;
+	}
+	
+	public enum UserStatus {
+		SUCCESS, ERROR;
 	}
 
 }
